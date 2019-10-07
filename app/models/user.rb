0b7951a40d797d524e4c2136ff62d1dfe2016b7f
Viodine <viodine@yahoo.com>
2019-10-07 11:52:03 +0200

    Made User tests successfully passing

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_secure_password
+  has_many :posts
+  has_many :comments
+  has_many :reviews
+  has_many :tickets
+
+  enum role: %i[user manager admin].freeze
+
+  validates :email,
+            format: { with: URI::MailTo::EMAIL_REGEXP },
+            presence: true,
+            uniqueness: { case_sensitive: false }
+  validates :password,
+            confirmation: true,
+            allow_nil: true,
+            length: { minimum: 8 }
+  validates :role,
+            presence: true,
+            inclusion: { in: roles.keys }
+  validates :coins_count,
+            presence: true,
+            numericality: { only_integers: true,
+                            greater_than_or_equal_to: 0 }
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe User, type: :model do
     it { should_not allow_value('user').for(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_presence_of(:password) }
-    it { should ensure_length_of(:password).is_at_least(8) }
-    it { should validate_presence_of(:password_confirmation) }
+    it { should validate_length_of(:password).is_at_least(8) }
+    it { should validate_confirmation_of(:password) }
     it { should validate_presence_of(:role) }
     it { should define_enum_for(:role).with_values(%i[user manager admin]) }
     it { should validate_presence_of(:coins_count) }
-    it { should validate_numericality_of(:coins_count).only_integer }
+    it { should validate_numericality_of(:coins_count).is_greater_than_or_equal_to(0) }
     it { should have_secure_password }
 
     it { should have_many(:posts) }
