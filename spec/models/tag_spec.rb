121dec44bf2624ccb2b335f906be9763af2513ef
Viodine <viodine@yahoo.com>
2019-10-07 13:22:18 +0200

    Added tests for Tag model

---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Tag, type: :model do
+  context 'valid tag' do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(2).is_at_most(20) }
+    it { should have_many(:posts_tags) }
+    it { should have_many(:posts).through(:posts_tags) }
+  end
+end
