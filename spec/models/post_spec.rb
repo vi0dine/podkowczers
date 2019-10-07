b0113b9039829375dc1a6ffd9aa8d54a8cb8273b
Viodine <viodine@yahoo.com>
2019-10-07 12:56:22 +0200

    Added tests for Post model

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  context 'valid post' do
+    it { should validate_presence_of(:title) }
+    it { should validate_uniqueness_of(:title).case_insensitive }
+    it { should validate_length_of(:title).is_at_least(3) }
+    it { should validate_presence_of(:body) }
+    it { should validate_length_of(:body).is_at_most(11_500) }
+    it { should belong_to(:user) }
+    it { should have_many(:posts_tags) } 
+    it { should have_many(:tags).through(:posts_tags) }
+  end
+end
