9f55613a8b01991b39f8c2cdb9dd66184fa99139
Viodine <viodine@yahoo.com>
2019-10-09 20:53:36 +0200

    Add tests for Concerts#show

--- spec/requests/concerts_requests_spec.rb
+++ spec/requests/concerts_requests_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe 'Concerts', type: :request do
     let!(:concerts) { create_list(:concert, 2) }
 
     before {
-      Concert.first.tickets.first.update(reserved: true)
       get '/api/v1/concerts'
     }
 
@@ -20,9 +19,31 @@ RSpec.describe 'Concerts', type: :request do
     it 'render all concerts data' do
       expect(json['data'].size).to eq(2)
     end
+  end
+
+  describe 'request single concert data' do
+    let!(:concerts) { create_list(:concert, 2) }
+    let(:concert_id) { concerts.first.id }
+
+    before {
+      Concert.find(concert_id).tickets.first.update(reserved: true)
+      get "/api/v1/concert/#{concert_id}" 
+    }
+
+    it 'respond with code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'respond with JSON' do
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+    end
+
+    it 'render concert data by given id' do
+      expect(json['data']['id']).to eq(concert_id)
+    end
 
     it 'render available tickets count' do
-      expect(json['data'].first['attributes']['tickets_count']).to eq(59)
+      expect(json['data']['attributes']['tickets_count']).to eq(59)
     end
   end
 end
