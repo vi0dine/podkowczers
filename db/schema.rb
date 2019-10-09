1bfe011a6cfa3cbb9059fd06de4fb779fb392fc6
Viodine <viodine@yahoo.com>
2019-10-09 10:37:30 +0200

    Switch to postgresql + add gems for testing

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_10_07_135503) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
