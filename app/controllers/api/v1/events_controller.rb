3108a5eb1f5f177ba6e62676e5a6a402693dfa68
Viodine <viodine@yahoo.com>
2019-10-10 20:20:17 +0200

    Add creating Events action

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,6 +2,9 @@
 module Api
   module V1
     class EventsController < ApplicationController
+      before_action :authorize_access_request!, only: %i[create]
+      before_action :admin?, only: %i[create]
+
       def index
         @events = Event.all
 
@@ -12,6 +15,23 @@ module Api
         @event = Event.find(params[:id])
 
         render json: EventSerializer.new(@event).serializable_hash
+      end
+
+      def create
+        @concert = Concert.find(params[:concert_id])
+        @event = @concert.events.new(event_params)
+
+        if @event.save
+          render json: EventSerializer.new(@event).serializable_hash, status: :created
+        else
+          render json: { error: concert.errors.full_messages.join(' ') }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def event_params
+        params.require(:event).permit(:place, :starts_at, :estimated_length)
       end
     end
   end
--- spec/requests/events_requests_spec.rb
+++ spec/requests/events_requests_spec.rb
@@ -54,4 +54,83 @@ RSpec.describe 'Events', type: :request do
       expect(json['data']['attributes']['starts_at']).to_not be_empty
     end
   end
+
+  describe 'request to add new event' do
+    let(:event) { attributes_for(:event) }
+    let(:concert) { create(:concert) }
+
+    context 'as an admin' do
+      let(:admin) { create(:user, :admin) }
+
+      before {
+        @tokens = session(admin)
+        cookies[JWTSessions.access_cookie] = @tokens[:access]
+        post '/api/v1/events', params: { event: event, concert_id: concert.id },
+                               headers: { JWTSessions.csrf_header.to_s => @tokens[:csrf].to_s }
+      }
+
+      it 'respond with code 201' do
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'respond with JSON' do
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+
+      it 'render new event data' do
+        expect(json['data']).to_not be_empty
+      end
+
+      let(:another_event) { attributes_for(:event) }
+
+      it 'add new event to db' do
+        expect do
+          post '/api/v1/events', params: { event: another_event, concert_id: concert.id },
+                                 headers: { JWTSessions.csrf_header.to_s => @tokens[:csrf].to_s }
+        end .to change { Event.all.count }.by(1)
+      end
+    end
+
+    context 'as a logged user (not admin)' do
+      let(:user) { create(:user) }
+
+      before {
+        @tokens = session(user)
+        cookies[JWTSessions.access_cookie] = @tokens[:access]
+        post '/api/v1/events', params: { event: event, concert_id: concert.id },
+                               headers: { JWTSessions.csrf_header.to_s => @tokens[:csrf].to_s }
+      }
+
+      it 'respond with code 401' do
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      let(:another_event) { attributes_for(:event) }
+
+      it 'did not add new event to db' do
+        expect do
+          post '/api/v1/events', params: { event: another_event, concert_id: concert.id },
+                                 headers: { JWTSessions.csrf_header.to_s => @tokens[:csrf].to_s }
+        end .to change { Event.all.count }.by(0)
+      end
+    end
+
+    context 'as a quest' do
+      before {
+        post '/api/v1/events', params: { event: event }
+      }
+
+      it 'respond with code 401' do
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      let(:another_event) { attributes_for(:event) }
+
+      it 'did not add new event to db' do
+        expect do
+          post '/api/v1/events', params: { event: another_event, concert_id: concert.id }
+        end .to change { Event.all.count }.by(0)
+      end
+    end
+  end
 end
