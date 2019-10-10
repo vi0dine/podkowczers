9e645a33e58efd96906b55a03fbeb469738ff42d
Viodine <viodine@yahoo.com>
2019-10-10 10:45:24 +0200

    Added specs for SignOut/Refresh requests

---
+++ spec/requests/refresh_request_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Refresh', type: :request do
+  describe 'refreshing user session' do
+    let(:user) { create(:user) }
+
+    before do
+      post '/signin', params: { email: user.email, password: user.password }
+      @old_token = json['csrf']
+      post '/refresh', headers: { 'X-CSRF-TOKEN': @old_token }
+    end
+
+    it 'respond with code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'respond with JSON' do
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+    end
+
+    it 'render csrf token' do
+      expect(json['csrf']).to_not be_empty
+    end
+
+    it 'render NEW csrf token' do
+      expect(json['csrf']).to_not eq(@old_token)
+    end
+  end
+end
---
+++ spec/requests/sign_out_request_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Sign out', type: :request do
+  describe 'signing out user' do
+    let(:user) { create(:user) }
+
+    before do
+      post '/signin', params: { email: user.email, password: user.password }
+      delete '/signout', headers: { 'X-CSRF-TOKEN': json['csrf'] }
+    end
+
+    it 'respond with code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'respond with JSON' do
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+    end
+
+    it 'render ok message' do
+      expect(json).to eq('ok')
+    end
+  end
+end
