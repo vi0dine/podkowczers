722ee38cd6c49fee03dfcaf6c20010917dcdaffd
Viodine <viodine@yahoo.com>
2019-10-10 19:58:26 +0200

    Add creating and deleting Concerts

--- app/controllers/api/v1/concerts_controller.rb
+++ app/controllers/api/v1/concerts_controller.rb
@@ -3,6 +3,9 @@
 module Api
   module V1
     class ConcertsController < ApplicationController
+      before_action :authorize_access_request!, only: %i[create destroy]
+      before_action :admin?, only: %i[create destroy]
+
       def index
         @concerts = Concert.all
         render json: ConcertSerializer.new(@concerts).serializable_hash
@@ -11,6 +14,22 @@ module Api
       def show
         @concert = Concert.find(params[:id])
         render json: ConcertSerializer.new(@concert).serializable_hash
+      end
+
+      def create
+        concert = Concert.new(concert_params)
+
+        if concert.save
+          render json: ConcertSerializer.new(concert).serializable_hash, status: :created
+        else
+          render json: { error: concert.errors.full_messages.join(' ') }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def concert_params
+        params.require(:concert).permit(:name, :description)
       end
     end
   end
--- spec/requests/concerts_requests_spec.rb
+++ spec/requests/concerts_requests_spec.rb
@@ -46,4 +46,87 @@ RSpec.describe 'Concerts', type: :request do
       expect(json['data']['attributes']['tickets_count']).to eq(59)
     end
   end
+
+  describe 'request to add new concert' do
+    let(:concert) { attributes_for(:concert) }
+
+    context 'as an admin' do
+      let(:admin) { create(:user, :admin) }
+
+      before {
+        @tokens = session(admin)
+        cookies[JWTSessions.access_cookie] = @tokens[:access]
+        post '/api/v1/concerts', params: { concert: concert },
+                                headers: { JWTSessions.csrf_header.to_s => @tokens[:csrf].to_s }
+      }
+
+      it 'respond with code 201' do
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'respond with JSON' do
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+
+      it 'render new concert data' do
+        expect(json['data']).to_not be_empty
+      end
+
+      let(:another_concert) { attributes_for(:concert) }
+
+      it 'add new concert to db' do
+        expect do
+          post '/api/v1/concerts', params: { concert: another_concert },
+                                   headers: { JWTSessions.csrf_header.to_s => @tokens[:csrf].to_s }
+        end .to change { Concert.all.count }.by(1)
+      end
+    end
+
+    context 'as a logged user (not admin)' do
+      let(:user) { create(:user) }
+
+      before {
+        @tokens = session(user)
+        cookies[JWTSessions.access_cookie] = @tokens[:access]
+        post '/api/v1/concerts', params: { concert: concert },
+                                 headers: { JWTSessions.csrf_header.to_s => @tokens[:csrf].to_s }
+      }
+
+      it 'respond with code 401' do
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      let(:another_concert) { attributes_for(:concert) }
+
+      it 'did not add new concert to db' do
+        expect do
+          post '/api/v1/concerts', params: { concert: another_concert },
+                                   headers: { JWTSessions.csrf_header.to_s => @tokens[:csrf].to_s }
+        end .to change { Concert.all.count }.by(0)
+      end
+    end
+
+    context 'as a quest' do
+
+      before {
+        post '/api/v1/concerts', params: { concert: concert }
+      }
+
+      it 'respond with code 401' do
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it 'respond with JSON' do
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+
+      let(:another_concert) { attributes_for(:concert) }
+
+      it 'did not add new concert to db' do
+        expect do
+          post '/api/v1/concerts', params: { concert: another_concert }
+        end .to change { Concert.all.count }.by(0)
+      end
+    end
+  end
 end
