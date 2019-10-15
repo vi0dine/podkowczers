4f57dd072dac3466a189dd6565adc318af57af70
Viodine <viodine@yahoo.com>
2019-10-15 12:21:50 +0200

    Add creating new posts action in PostsController

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,6 +3,9 @@
 module Api
   module V1
     class PostsController < ApplicationController
+      before_action :authorize_access_request!, only: %i[create]
+      before_action :admin?, only: %i[create]
+
       def index
         @posts = Post.all
 
@@ -13,6 +16,23 @@ module Api
         @post = Post.find(params[:id])
 
         render json: PostSerializer.new(@post).serializable_hash, status: :ok
+      end
+
+      def create
+        @user = current_user
+        @post = @user.posts.new(post_params)
+
+        if @post.save
+          render json: PostSerializer.new(@post).serializable_hash, status: :created
+        else
+          render json: { error: @post.errors.full_messages.join(' ') }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def post_params
+        params.require(:post).permit(:title, :body)
       end
     end
   end
--- spec/requests/posts_requests_spec.rb
+++ spec/requests/posts_requests_spec.rb
@@ -45,4 +45,82 @@ RSpec.describe 'Posts', type: :request do
       expect(json['data']['relationships']['comments']['data'].size).to eq(posts.first.comments.size)
     end
   end
+
+  describe 'request to add new post' do
+    let(:new_post) { attributes_for(:post) }
+
+    context 'as an admin' do
+      let(:admin) { create(:user, :admin) }
+
+      before {
+        @tokens = session(admin)
+        cookies[JWTSessions.access_cookie] = @tokens[:access]
+        post '/api/v1/posts', params: { post: new_post },
+                              headers: { JWTSessions.csrf_header.to_s => @tokens[:csrf].to_s }
+      }
+
+      it 'respond with code 201' do
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'respond with JSON' do
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+
+      it 'render new post data' do
+        expect(json['data']).to_not be_empty
+      end
+
+      let(:another_post) { attributes_for(:post) }
+
+      it 'add new post to db' do
+        expect do
+          post '/api/v1/posts', params: { post: another_post },
+                                 headers: { JWTSessions.csrf_header.to_s => @tokens[:csrf].to_s }
+        end .to change { Post.all.count }.by(1)
+      end
+    end
+
+    context 'as a logged user (not admin)' do
+      let(:user) { create(:user) }
+
+      before {
+        @tokens = session(user)
+        cookies[JWTSessions.access_cookie] = @tokens[:access]
+        post '/api/v1/posts', params: { post: new_post },
+                              headers: { JWTSessions.csrf_header.to_s => @tokens[:csrf].to_s }
+      }
+
+      it 'respond with code 401' do
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      let(:another_post) { attributes_for(:post) }
+
+      it 'did not add new post to db' do
+        expect do
+          post '/api/v1/posts', params: { post: another_post },
+                                 headers: { JWTSessions.csrf_header.to_s => @tokens[:csrf].to_s }
+        end .to change { Post.all.count }.by(0)
+      end
+    end
+
+    context 'as a quest' do
+      before {
+        post '/api/v1/events', params: { post: new_post }
+      }
+
+      it 'respond with code 401' do
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      let(:another_post) { attributes_for(:post) }
+
+      it 'did not add new post to db' do
+        expect do
+          post '/api/v1/posts', params: { post: another_post }
+        end .to change { Post.all.count }.by(0)
+      end
+    end
+  end
 end
