69dabfd4c0e30b7b56fd82411d0a8e8b7a3436f4
Viodine <viodine@yahoo.com>
2019-10-18 13:08:46 +0200

    Quickfix

--- config/initializers/jwt_sessions.rb
+++ config/initializers/jwt_sessions.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
-JWTSessions.encryption_key = Rails.application.credentials.secret_key_base
+JWTSessions.encryption_key =
+  Rails.env.production? ? ENV['SECRET_KEY_BASE'] : Rails.application.credentials.secret_key_base
