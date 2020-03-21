b6a51b413c24d1074ac6cc4e7ae465c3c1e9557d
viodine <viodine@yahoo.com>
2020-03-21 11:46:50 +0100

    Frontend cleanup

--- frontend/layouts/error.vue
+++ frontend/layouts/error.vue
@@ -1,44 +1,49 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <v-container fluid>
+      <v-row align="center" justify="center" style="height: 100vh;">
+        <v-col cols="12">
+          <h1 v-if="error.statusCode === 404">
+            {{ pageNotFound }}
+          </h1>
+          <h1 v-else>
+            {{ otherError }}
+          </h1>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script>
-export default {
-  layout: 'empty',
-  props: {
-    error: {
-      type: Object,
-      default: null
-    }
-  },
-  data () {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
-  },
-  head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
+  export default {
+    layout: 'empty',
+    props: {
+      error: {
+        type: Object,
+        default: null
+      }
+    },
+    data() {
+      return {
+        pageNotFound: 'Błąd 404',
+        otherError: 'Wystąpił błąd'
+      }
+    },
+    head() {
+      const title =
+        this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+      return {
+        title
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-h1 {
-  font-size: 20px;
-}
+  h1 {
+    font-family: 'Muli', sans-serif;
+    color: #f8f8f8;
+    font-size: 70px;
+  }
 </style>
