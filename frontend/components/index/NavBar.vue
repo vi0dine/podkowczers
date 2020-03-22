5933652ccec3e090b1f6dcec6a6cdf5a8590aba6
viodine <viodine@yahoo.com>
2020-03-22 13:40:02 +0100

    SEO etc.

---
+++ frontend/components/index/NavBar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="top-bar">
+    <div class="logo-container">
+      <img alt="logo" src="logo.png" class="logo" />
+    </div>
+    <div class="nav-menu">
+      <div class="soon">
+        WKRÓTCE
+      </div>
+      <NuxtLink to="/" class="nav-link">
+        <div>ZAŁÓŻ KONTO</div>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .top-bar {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    height: 9vh;
+    width: 100vw;
+    z-index: 10;
+  }
+  .logo-container {
+    display: flex;
+    flex: .2;
+  }
+  .logo {
+    padding: 1vh;
+    height: 8vh;
+    width: auto;
+  }
+  .nav-menu {
+    display: flex;
+    flex: .4;
+    justify-content: flex-end;
+    align-items: center;
+    margin: 0 30px 0 10px;
+  }
+  .nav-link {
+    background-color: $MUSTARD;
+    border-radius: 10px;
+    padding: 15px;
+    color: $PURPLE;
+    font-family: $main-font;
+    text-decoration: none;
+  }
+  .soon {
+    position: absolute;
+    right: 40px;
+    top: 20px;
+    border-radius: 5px;
+    padding: 5px 15px 5px 15px;
+    background-color: $RED;
+    border: 3px solid $WHITE;
+    font-family: $main-font;
+    font-weight: bold;
+    color: $WHITE;
+    transform: rotate(20deg);
+  }
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,25 +1,69 @@
 <template>
-  <v-layout>
-    <v-container fluid>
-      <v-row align="center" justify="center" style="height: 100vh;">
-        <v-col cols="12">
-          <div class="temp-text">Już wkrótce...</div>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-layout>
+  <div>
+    <NavBar/>
+    <div class="content"
+         style="background:linear-gradient(45deg, rgba(152, 68, 71, 0.2) 35%, rgba(95, 15, 64, 0.9)), url('bg.jpg')">
+      <div class="intro-container">
+        <div class="intro-text">
+          Oficjalna strona zespołu DePodkówczers z&nbsp;Wałbrzycha
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+  import NavBar from '~/components/index/NavBar.vue'
 
-}
+  export default {
+    components: {
+      NavBar: NavBar
+    },
+    head() {
+      return {
+        title: 'DePodkówczers - Oficjalna strona internetowa',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'Oficjalna strona internetowa zespołu DePodkówczers z Wałbrzycha'
+          }
+        ]
+      }
+    }
+  }
 </script>
 
-<style>
-  .temp-text {
-    color: #f8f8f8;
-    font-size: 36px;
-    font-family: 'Muli', sans-serif;
+<style scoped lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Kalam:700&display=swap');
+
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    height: 100vh;
+    width: 100vw;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: flex-end;
+  }
+
+  .intro-container {
+    flex: .3;
+    width: 50vw;
+    margin-right: 5vw;
+    padding: 50px;
+  }
+
+  .intro-text {
+    -webkit-text-stroke: 2px $MUSTARD;
+    -webkit-text-fill-color: $RED;
+    text-align: right;
+    font-family: 'Kalam', cursive;
+    font-weight: bold;
+    font-size: 56pt;
   }
 </style>
