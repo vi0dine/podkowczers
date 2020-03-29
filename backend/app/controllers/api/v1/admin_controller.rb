f19a371c28d02b578a8d3464bd3d121ab9e36085
viodine <viodine@yahoo.com>
2020-03-29 14:11:57 +0200

    Quickfix

--- backend/app/controllers/api/v1/admin_controller.rb
+++ backend/app/controllers/api/v1/admin_controller.rb
@@ -2,10 +2,6 @@ module Api
   module V1
     class AdminController < ApplicationController
       before_action :doorkeeper_authorize!
-
-      def stats
-        @stats =
-      end
     end
   end
 end
